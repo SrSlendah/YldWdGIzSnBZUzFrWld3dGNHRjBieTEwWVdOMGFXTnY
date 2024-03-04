0747fd31e31a483278c711e9d460e6e4dec96824
Slendah <56037403+SrSlendah@users.noreply.github.com>
2024-03-04 22:55:10 +0100

    Add files via upload

---
+++ js/redirect.js
@@ -0,0 +1,79 @@
+function goTo(string) {
+
+//wiki-link
+
+    if (string == 'wjb') {
+        window.location.href = "wiki/wiki-jb"
+    }
+
+    if (string == 'wbh') {
+        window.location.href = "wiki/wiki-bh"
+    }
+
+
+
+
+
+
+
+// Manga
+
+    if (string == 'manga_jojo') {
+        window.location.href = "../lista-manga/jojos-bizarre-adventure"
+    };
+
+    if (string == 'manga_nanatsu') {
+        window.location.href = "../lista-manga/nanatsu-no-taizai"
+    };
+
+    if (string == 'manga_onepiece') {
+        window.location.href = "../lista-manga/one-piece"
+    };
+
+    if (string == 'manga_sololvl') {
+        window.location.href = "../lista-manga/solo-leveling"
+    };
+
+//Anime
+
+    if (string == 'anime_jojo') {
+        window.location.href = "../lista-anime/jojos-bizarre-adventure"
+    };
+
+    if (string == 'anime_nanatsu') {
+        window.location.href = "../lista-anime/nanatsu-no-taizai"
+    };
+
+    if (string == 'anime_onepiece') {
+        window.location.href = "../lista-anime/one-piece"
+    };
+
+    if (string == 'anime_sololvl') {
+        window.location.href = "../lista-anime/solo-leveling"
+    };
+
+    if (string == 'anime_deathnote') {
+        window.location.href = "../lista-anime/death-note"
+    };
+
+//Lista Peliculas y series
+
+    if (string == 'lps_onepiece') {
+        window.location.href = "../lista-peliculas-series/one-piece"
+    };
+
+    if (string == 'lps_deathnote') {
+        window.location.href = "../lista-peliculas-series/death-note"
+    };
+
+//Wiki Brawlhalla
+
+    if (string == 'wbh_collectors') {
+        window.open('https://store.steampowered.com/app/298641/Brawlhalla__Collectors_Pack/', '_blank')
+    };
+
+    if (string == 'wbh_yetee') {
+        window.open('https://theyetee.com/collections/brawlhalla', '_blank')
+    };
+
+}
